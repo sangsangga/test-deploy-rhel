7037443810730a1133d28b2fabd45369e3afe179
sangsangga <wandaputrasangga@gmail.com>
2021-03-11 22:33:07 +0700

    add env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,23 @@ module.exports = {
     {
       name: "Orchestrator",
       script: "cd orchestrator && npm install && node app.js",
+      env: {
+        PORT: 4000,
+      },
     },
     {
       name: "Services-Customer",
       script: "cd services && cd customer && npm install && node app.js",
+      env: {
+        PORT: 4001,
+      },
     },
     {
       name: "Services-Product",
       script: "cd services && cd product && npm install && node app.js",
+      env: {
+        PORT: 4002,
+      },
     },
   ],
 };
